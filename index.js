2e00af06b8c598e2368d989ebfcc92f1b70b8061
Zech Drinkhall <zechdrink@aol.com>
2019-03-26 13:30:11 -0700

    added knexfile and created first get endpoint

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+
+const knexConfig = require('./knexfile.js')
 
 const server = express();
+
+//connect to database here!
+const db = knex(knexConfig.development);
+
+server.get('/', (req, res) => {
+  res.send('api working')
+});
+
+//Get endpoint
+server.get('/api/zoos', (req, res) => {
+  db('zoos').then(zoos => {
+    res.status(200).json(zoos);
+  }).catch(err => {res.status(500).json(err)});
+})
+
+
 
 server.use(express.json());
 server.use(helmet());
