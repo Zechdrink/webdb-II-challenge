b589b5c07e2a014bf5a055f57440ee24a2ff71c9
Zech Drinkhall <zechdrink@aol.com>
2019-03-26 14:42:24 -0700

    finished the post, delete and update endpoints

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const knexConfig = require('./knexfile.js')
 
 const server = express();
 
+server.use(express.json())
 //connect to database here!
 const db = knex(knexConfig.development);
 
@@ -13,12 +14,55 @@ server.get('/', (req, res) => {
   res.send('api working')
 });
 
-//Get endpoint
+//Get List of Zoos endpoint
 server.get('/api/zoos', (req, res) => {
   db('zoos').then(zoos => {
     res.status(200).json(zoos);
   }).catch(err => {res.status(500).json(err)});
 })
+
+//Get zoo by ID
+server.get('/api/zoos/:id', (req, res) =>{
+  const zoo =
+  db('zoos')
+
+})
+
+
+
+//Post a zoo
+server.post('/api/zoos', (req, res) => {
+  db('zoos')
+    .insert(req.body)
+    .then(id => {
+      res.status(201).json(id);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+//Delete a zoo
+server.delete('/api/zoos/:id', (req, res) => {
+  db('zoos')
+    .where({ id: req.params.id })
+    .delete()
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(err =>  res.status(500).json(err))
+})
+
+//Update a zoo
+server.put('/api/zoos/:id', (req, res) => {
+  const changes = req.body;
+  db('zoos')
+    .where({ id: req.params.id })
+    .update(changes)
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(err => res.status(500).json(err));
+})
+
 
 
 
